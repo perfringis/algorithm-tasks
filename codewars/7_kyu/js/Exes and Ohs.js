fc1b67a82191534f908847f10d52f2bf70817bff
dzidzi <kopko.mateusz@gmail.com>
2022-10-27 19:59:57 +0200

    resolve task

---
+++ codewars/7_kyu/js/Exes and Ohs.js
@@ -0,0 +1,12 @@
+function XO(str) {
+    let x = 0;
+    let o = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === 'x') {
+            x++;
+        } else if (str[i].toLowerCase() === 'o') {
+            o++;
+        }
+    }
+    return x === o;
+}
