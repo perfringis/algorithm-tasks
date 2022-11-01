38225514de367f7b3f29052fb82d6388643c878f
dzidzi <kopko.mateusz@gmail.com>
2022-11-01 11:56:11 +0100

    resolve task

--- codewars/5_kyu/js/Extract the domain name from a URL.js
+++ codewars/5_kyu/js/Extract the domain name from a URL.js
@@ -1,5 +1,12 @@
-const domainName = url => {
-    
-};
+const domainName = (url) => {
+  // remove http(s)://
+  url = url.replace(/(https?:\/\/)/, "");
 
-//  TODO
+  // remove www.
+  url = url.replace(/(www\.)/, "");
+
+  // remove everything after the first /
+  url = url.replace(/(\/.*)/, "");
+
+  return url.split('.')[0];
+};
